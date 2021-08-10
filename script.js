487db928f0ef5deefc32e9caa659256674301e7a
Christopher Lee <69568732+christophermlee2@users.noreply.github.com>
2021-08-09 21:46:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -27,3 +27,27 @@ window.addEventListener('click', function(e) {
         heroElement.classList.remove('is-open');
     }
 });
+
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+    }).then(response => {
+    status.innerHTML = "Thanks for your submission!";
+    status.classList.add('success');
+    form.reset()
+    }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+    status.classList.add('error');
+    });
+}
+form.addEventListener("submit", handleSubmit)
