70ee5e6c0f286112892d8508675f003ed2801e4b
christophermlee2 <christophermlee2@gmail.com>
2023-12-27 11:05:28 -0500

    Remove Contact Info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        // @astrojs/react renderer
-        react(), 
-        // @astrojs/vue renderer
-        vue()],
-    site: 'https://www.choicespecs.com'
+  integrations: [
+    // @astrojs/react renderer
+    react(),
+    // @astrojs/vue renderer
+    vue(),
+  ],
+  site: "https://www.choicespecs.com",
 });
